090bed432b2fe963134ce3dfd870bb7fefbf277f
MAHIPALKING9 <78957205+MAHIPALKING9@users.noreply.github.com>
2021-05-05 16:00:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas,backgroundImage,playerCount,database,form,player;
+var game;
+var gameState = 0;
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+function draw(){
+  if(playerCount===4){
+    game.update(1);
+  }
+  if(gameState===1){
+    clear();
+    game.play();
+  }
+}
